83ef570cdda90b37ea5da679d30c4a0acc9d6de4
root <root@LAPTOP-E01KR4V4.localdomain>
2022-03-28 22:54:58 +0100

    binary-search

---
+++ algorithms.py
@@ -0,0 +1,17 @@
+def binary_search(searchlist, item):
+
+    first = 0
+    last = len(searchlist) - 1
+    middle = round((first + last) / 2)
+
+    while first <= last:
+        if searchlist[middle] == item:
+            return middle
+        else:
+            if searchlist[middle] > item:
+                last = middle - 1
+                middle = round((first + last) / 2)
+            elif searchlist[middle] < item:
+                first = middle + 1
+                middle = round((first + last) / 2)
+    return None
